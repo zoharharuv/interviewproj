8104fd5b2d85df76d3b43af912001eeae66cdc32
Zohar H <misterblat@gmail.com>
2021-10-19 13:40:27 +0300

    header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -3,8 +3,8 @@ import { Component } from "react";
 export class AppHeader  extends Component{
     render(){
         return(
-            <section className="app-header">
-                    <h1>HEADER</h1>
+            <section className="app-header flex center-center">
+                    <h1>PlayList</h1>
             </section>
         )
     }
